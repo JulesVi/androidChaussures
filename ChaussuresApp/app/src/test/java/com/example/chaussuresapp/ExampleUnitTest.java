394ee3fdee9c203a8c4852f1a1778fa221880e82
Jules <jujul.vivi@gmail.com>
2019-12-12 18:24:38 +0100

    tests

--- ChaussuresApp/app/src/test/java/com/example/chaussuresapp/ExampleUnitTest.java
+++ ChaussuresApp/app/src/test/java/com/example/chaussuresapp/ExampleUnitTest.java
@@ -1,6 +1,15 @@
 package com.example.chaussuresapp;
 
+import android.os.StrictMode;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +20,39 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    // permet de tester la forme de l'url (si il est bien créé) et 200 s'il retourne bien un lien
+    public void imageValidator_CorrectLinkSimple_ReturnsTrue() {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            URL url = new URL("https://scene7.zumiez.com/is/image/zumiez/pdp_hero/New-Balance-Numeric-306-Foy-Blue-%26-Red-Skate-Shoes-_320281-front-US.jpg");
+            HttpsURLConnection huc = (HttpsURLConnection) url.openConnection();
+            int responseCode = huc.getResponseCode();
+            Assert.assertEquals(HttpsURLConnection.HTTP_OK, responseCode);
+        } catch (IOException e) {}
     }
+
+    @Test
+    public void imageValidator_InvalidUrlNoHttp_ReturnFalse() {
+        boolean imgExist = false;
+        try {
+            URL url = new URL("scene7.zumiez.com/is/image/zumiez/pdp_hero/New-Balance-Numeric-306-Foy-Blue-%26-Red-Skate-Shoes-_320281-front-US.jpg");
+            imgExist = true;
+        } catch (MalformedURLException e) {
+        }
+        assertFalse(imgExist);
+    }
+
+    @Test
+    public void imageValidator_NullUrl_ReturnFalse() {
+        boolean imgExist = false;
+        try {
+            URL url = new URL("");
+            imgExist = true;
+        } catch (MalformedURLException e) {
+        }
+        assertFalse(imgExist);
+    }
+
+
 }
