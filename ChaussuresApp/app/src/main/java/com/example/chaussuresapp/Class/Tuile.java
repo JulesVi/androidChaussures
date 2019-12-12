8998373b9206f8fdf7a67416d586bc56d2188a64
Jules <jujul.vivi@gmail.com>
2019-12-12 16:13:25 +0100

    modif constructeur

--- ChaussuresApp/app/src/main/java/com/example/chaussuresapp/Class/Tuile.java
+++ ChaussuresApp/app/src/main/java/com/example/chaussuresapp/Class/Tuile.java
@@ -24,36 +24,37 @@ public class Tuile implements Serializable {
     private int cp; // code postal
     private int prix;
 
-    public Tuile(String imgId, String titreAnnonce, String auteurAnnonce, String description, String pied, int taille, String etat, String localisation, int cp, int prix ) {
-        this.imgId = imgId;
-        this.titreAnnonce = titreAnnonce;
-        this.auteurAnnonce = auteurAnnonce;
-        this.description = description;
-        this.pied = pied;
-        this.taille = taille;
-        this.etat = etat;
-        this.localisation = localisation;
-        this.cp = cp;
-        this.prix = prix;
-    }
+//    public Tuile(String imgId, String titreAnnonce, String auteurAnnonce, String description, String pied, int taille, String etat, String localisation, int cp, int prix) {
+//        this.imgId = imgId;
+//        this.titreAnnonce = titreAnnonce;
+//        this.auteurAnnonce = auteurAnnonce;
+//        this.description = description;
+//        this.pied = pied;
+//        this.taille = taille;
+//        this.etat = etat;
+//        this.localisation = localisation;
+//        this.cp = cp;
+//        this.prix = prix;
+//    }
 
     public Tuile(QueryDocumentSnapshot document) {
         this.imgId = document.get("image").toString();
         this.titreAnnonce = document.get("titre").toString();
         this.auteurAnnonce = document.getId();
-        this.pied = "pied en dur à modifier";
+        this.description = document.get("description").toString();
+        this.pied = document.get("pied").toString();
         this.taille = new Integer(document.get("taille").toString());
-        this.etat = "etat en dur";
-        this.localisation = "localisation en dur";
-        this.cp = 38100;
+        this.etat = document.get("etat").toString();
+        this.localisation = document.get("localisation").toString();
+        this.cp = new Integer(document.get("cp").toString());
+        this.prix = new Integer(document.get("prix").toString());
     }
 
-    public Bitmap getImgId() throws IOException {
+    public Bitmap getImgBmp() throws IOException {
         Bitmap bmp = null;
         try {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
-
             //fonctionne uniquement avec les images HTTPS
             URL url = new URL(imgId);
             bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
